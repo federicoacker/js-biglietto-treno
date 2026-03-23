b328168c22d7dd065338859337813a0699298633
federicoacker <acker.federico@gmail.com>
2026-03-23 14:31:32 +0100

    Aggiunto il parsing a float per i kilometri e a int per l'età e aggiunto il blocco condizionale per il calcolo del prezzo scontato

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
-const BasePriceKm = 0.21;
-const minorSale = 20;
-const seniorSale = 40;
+const basePriceKm = 0.21;
+const minorSale = 0.2;
+const seniorSale = 0.4;
+const minorAge = 18;
+const seniorAge = 65;
+let finalPrice = 0;
 
 let kilometers = 0;
 let age = 0;
@@ -10,7 +13,17 @@ function buttonClick() {
     
     kilometers = trainFormInputs[0].value;
     age = trainFormInputs[1].value;
-    
-    console.log(kilometers, age);
 }
+
+kilometers = parseFloat(kilometers);
+age = parseInt(age);
+
+finalPrice = basePriceKm * kilometers;
+
+if(!(isNaN(age)) && age < minorAge){
+    finalPrice -= finalPrice*minorSale;
+} else if (!(isNaN(age)) && age >= seniorAge){
+    finalPrice -= finalPrice*seniorSale;
+}
+
 
