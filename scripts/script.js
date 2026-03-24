7e14052a08ef3f7faecb667474f964ff088043a1
federicoacker <acker.federico@gmail.com>
2026-03-24 10:35:02 +0100

    Semplificato logica per la validazione dati

--- scripts/script.js
+++ scripts/script.js
@@ -6,21 +6,20 @@ const minorAge = 18; // Soglia di età per essere considerato minorenne
 const seniorAge = 65; // Soglia di età per essere considerato senior
 
 // DICHIARAZIONE VARIABILI E PROMPT DI RICHIESTA ALL'UTENTE CON OPERATORE TERNARIO PER VALIDARE I DATI //
-let isKilometersValid = false; // Flag usata in seguito per validazione dell'input
-let isAgeValid = false; // Flag usata in seguito per validazione dell'input
+let isKilometersValid; // Flag usata in seguito per validazione dell'input
+let isAgeValid; // Flag usata in seguito per validazione dell'input
 let finalPrice = 0; // Prezzo finale
 
 const kilometers = Number(prompt("Quanti chilometri vuoi viaggiare?"));
 
-//Operatore ternario, controlla se kilometers è NaN o kilometers è <= 0, in quel caso da errore, altrimenti setta isKilometersValid a true
-(isNaN(kilometers) || kilometers <= 0) ? console.error("Il valore inserito per i chilometri non è valido")
-: isKilometersValid = true;
+isKilometersValid = (isNaN(kilometers) || kilometers <= 0);
 
 const age = Number(prompt("Quanti anni hai"));
 
-//Operatore ternario, controlla se age è NaN o age è <= 0, in quel caso da errore, altrimenti setta isAgeValid a true
-(isNaN(age) || age <= 0) ? console.error("Il valore inserito per l'età non è valido")
-: isAgeValid = true;
+isAgeValid = (isNaN(age) || age <=0);
+
+if(!isKilometersValid) { console.error("Il valore inserito per i chilometri non è valido");}
+if(!isAgeValid) { console.error("Il valore inserito per l'età non è valido");}
 
 //Avendo usato delle flag booleane per controllare la validità dei dati inseriti, il controllo è più semplice da scrivere;
 if(isAgeValid && isKilometersValid){
